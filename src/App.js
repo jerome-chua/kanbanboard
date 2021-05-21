ec374e628623e71fadd9173bb5a2b130d0a8e71b
Jchu4 <jerome.chuame@gmail.com>
2021-05-21 21:25:21 +0800

    Basic dnd board from react examples

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import KanbanBoard from '../src/components/KanbanBoard.jsx'
 
 function App() {
   return (
     <div className="App">
-      
+      <KanbanBoard />
     </div>
   );
 }
